1a98275fd10841bcc821eff0ff9c60ddd5318a7e
GerzsenyiRoland <103255961+GerzsenyiRoland@users.noreply.github.com>
2022-05-02 17:28:40 +0200

    Kész a Ital hozzáadása és kilistázása

--- src/main.java
+++ src/main.java
@@ -2,37 +2,57 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main{
-     Scanner sc = new Scanner(System.in);
+     static Scanner sc = new Scanner(System.in);
 
-     public void main(String agrs[]){
+     public static void main(String agrs[]){
+        String kezdo[] = {"1.Ital hozzáadása","2.Ital törlése","3.Italok kilisztázása","4.Recept létrehozáse","5.Recept törlése","6.Receptek kilistázása","7.Kilépés és mentés","8.Kilépés mentés nélkül"};
         ArrayList<Ital> italok = new ArrayList<Ital>();
         ArrayList<Recept> receptek = new ArrayList<Recept>();
-        italok = addItal(italok);
+        while(true){
+            int a = segit(kezdo);
+            if (a==1){italok.add(addItal());
+            }else if(a==2){
+            }else if (a==3){iratItal(italok);
+            }else if (a==4){
+            }else if (a==5){
+            }else if (a==6){
+            }else if (a==7){
+            }else if (a==8){}
+        }
     }
+    
 
-    public int segit(int k){
+    public static int segit(String ku[]){
+        for (int i=0;i<ku.length;i++){
+            System.out.println(ku[i]);
+        }
         while (true){
-            System.out.println("Adja meg a választását");
+            System.out.print("Adja meg a választását: ");
             try{
             int a = sc.nextInt();
-            if (a<=k){return a;} else{System.out.println("A felsorolt számokból adjon meg.");}
+            if (a<=ku.length-1 && a >0){return a;} else{System.out.println("A felsorolt számokból adjon meg.");}
             }catch(Exception a){System.out.println("Számotadjon meg!");}
         }
     }
 
-    public ArrayList<Ital> addItal(ArrayList<Ital> hoz){
+    public static Ital addItal(){
         while (true){
             System.out.println("Adja meg az ital nevét fajtáját és fokát alsó vonásal elválasztva'_'.(Pl:Borsodi_Ipa_4)");
-            String dod = sc.nextLine();
+            String dod = sc.next();
             String dodo[] = dod.split("_");
             if (dodo.length<3){System.out.println("Nem adot meg elég adatot vagy helytelen formátumot használt.");}else{
                 try{
                     Ital u = new Ital(dodo[0],dodo[1], Double.parseDouble(dodo[2]));
-                    hoz.add(u);
-                    return hoz;
+                    return u;
                 }catch(Exception a){System.out.println("Számot adjon meg az utolsó adatnak.");}
             }
 
+        }
+    }
+
+    public static void iratItal(ArrayList<Ital> a){
+        for (int i=0;i< a.size();i++){
+            System.out.println(a.get(i).toString());
         }
     }
 }
